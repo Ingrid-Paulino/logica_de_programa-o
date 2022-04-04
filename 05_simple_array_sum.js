b2d5a5bd0ebf5fac0b7ef356652e3afda91d84b4
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-04 10:13:07 -0300

    C6 - adicionado uma segunda forma de resolver o exercicio 5

--- 05_simple_array_sum.js
+++ 05_simple_array_sum.js
@@ -2,11 +2,23 @@
 
 // For example, if the array ar=[1, 2, 3] , 1+2+3=6 , so return 6.
 
-function simpleArraySum(ar) {
+function simpleArraySum1(ar) {
   // Write your code here
- return ar.reduce((n, arr) => n + arr, 0)
+ return ar.reduce((n, arr) => n + arr, 0);
 }
 
-console.log(simpleArraySum([1, 2, 3]));
+// ou
 
-console.log(simpleArraySum([10, 20, 30, 5]));
+function simpleArraySum2(ar) {
+  // Write your code here
+  let soma = 0;
+ for (let index=0; index < ar.length; index++) {
+    soma += ar[index];
+ }
+ return soma;
+}
+
+console.log('simpleArraySum1:', simpleArraySum1( [1, 2, 3]));
+console.log('simpleArraySum1:', simpleArraySum1( [10, 20, 30, 50]));
+console.log('simpleArraySum2:', simpleArraySum2([1, 2, 3]));
+console.log('simpleArraySum2:', simpleArraySum2( [10, 20, 30, 5]));
