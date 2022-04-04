c0e5117ffcc46cefcd25bb0bd3269a12adf96c50
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-04 09:41:40 -0300

    C5 - Exercicio 4: resolvido

---
+++ 04_solve_me_first.js
@@ -0,0 +1,26 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+
+// Example
+  // a=7
+  // b=3
+
+// Return 10.
+
+// Function Description
+// Complete the solveMeFirst function in the editor below.
+
+// solveMeFirst has the following parameters:
+
+// int a: the first value
+// int b: the second value
+// Returns
+// - int: the sum of a  and b
+
+function solveMeFirst(a, b) {
+  // Hint: Type return a+b below
+  const calculo = a + b;
+  console.log(`Soma: ${calculo}`);   
+  return calculo
+}
+
+solveMeFirst(7, 3);
