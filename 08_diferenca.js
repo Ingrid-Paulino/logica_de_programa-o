51516d4e7a4f79974466d07e23f8c7aba1c594f4
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-07 19:24:10 -0300

    C10 - Exercicio 8: resolvido

---
+++ 08_diferenca.js
@@ -0,0 +1,11 @@
+/* Dado um número X, retorne a diferença entre X e 10 de forma que o resultado seja sempre um valor positivo. 
+Isto é: 
+  •se X < 10, faça 10-X, por exemplo, se X = 3, retorne 7 (10-3)
+  • se X> 10, faça X-10, por exemplo, se X = 15, retorne 5 (15-10) */
+
+function diferenca(x) {
+  if (x < 10) console.log(10 - x); 
+  if (x > 10) console.log(x - 10);
+}
+
+diferenca(20)
