b2498bb9d8082950fe0e5185393d36050e4ac5d0
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-04-07 20:28:59 -0300

    Outras formas de resolve o requisito 8

--- 08_diferenca.js
+++ 08_diferenca.js
@@ -3,9 +3,25 @@ Isto é:
   •se X < 10, faça 10-X, por exemplo, se X = 3, retorne 7 (10-3)
   • se X> 10, faça X-10, por exemplo, se X = 15, retorne 5 (15-10) */
 
+// function diferenca(x) {
+//   if (x < 10) console.log(10 - x); 
+//   if (x > 10) console.log(x - 10);
+// }
+
+// ou
+
+// function diferenca(x) {
+//   if (x < 10) {
+//     console.log(10 - x); 
+//   } else {
+//     console.log(x - 10);
+//   }
+// }
+
+// ou
+
 function diferenca(x) {
-  if (x < 10) console.log(10 - x); 
-  if (x > 10) console.log(x - 10);
+  x < 10 ? console.log(10 - x) : console.log(x - 10);
 }
 
 diferenca(20)
