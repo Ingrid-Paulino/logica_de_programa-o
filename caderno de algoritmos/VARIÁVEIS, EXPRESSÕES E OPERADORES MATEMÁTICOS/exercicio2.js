be37ea552f4f6d5418cef7d3683b2bbe67b73ad5
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-07-08 07:51:46 -0300

    Adicionado exercicio 2 do caderno de algoritmos -  VARIÁVEIS, EXPRESSÕES E OPERADORES MATEMÁTICOS - exercicio sobre pseudocodigo

---
+++ caderno de algoritmos/VARIÁVEIS, EXPRESSÕES E OPERADORES MATEMÁTICOS/exercicio2.js
@@ -0,0 +1,70 @@
+// Escreva em pseudocódigo, as seguintes expressões matemáticas:
+/*
+  a) (A + B)*C
+  b) TOTAL / N
+  c) A - B(C + D 2 ) / E
+  d) base expoente
+  e ) a * b c
+*/
+//  ---------------------------------RESULTS------------------------------------------
+
+// a)
+  /*
+    inicio
+    var A, B, C, RESULT
+    ler(A)
+    ler(B)
+    ler(C)
+    RESULT <- (A + B)*C
+    escrever(RESULT)
+    fim
+  */
+
+// b)
+  /*
+    inicio
+    var TOTAL, N, RESULT
+    ler(TOTAL)
+    ler(N)
+    RESULT <- TOTAL/N
+    escrever(RESULT)
+    fim
+  */
+
+// c)
+  /*
+    inicio
+    var A, B, C, D, E, 2, RESULT
+    ler(A)
+    ler(B)
+    ler(C)
+    ler(D)
+    ler(E)
+    ler(2)
+    RESULT <- A - B(C + D^2)/E
+    escrever(RESULT)
+    fim
+  */
+
+// d)
+  /*
+    inicio
+    var BASE, EXPONETE
+    ler(BASE)
+    ler(EXPONETE)
+    RESULT <- BASE^EXPOENTE
+    escrever(RESULT)
+    fim
+  */
+
+// e)
+  /*
+    inicio
+    var A, B, C
+    ler(A)
+    ler(B)
+    ler(C)
+    RESULT <- A * B^C
+    escrever(RESULT)
+    fim
+  */
